9d8bf73ac9d234de1cefd8a73cd6e0beb173c127
William Sayre <wjs9ej@virginia.edu>
2020-09-09 12:44:59 -0400

    Updates

---
+++ precache-manifest.6140416279dd716e1f36e43e9ebefa85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03987fce4740550f9e03e733ea322fa4",
+    "url": "/wjs97/wjs97.github.io/blackjack/index.html"
+  },
+  {
+    "revision": "475888dcf99c1ec9fa4a",
+    "url": "/wjs97/wjs97.github.io/blackjack/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "bcf8476f0b397ebf56a7",
+    "url": "/wjs97/wjs97.github.io/blackjack/static/css/main.1c846a01.chunk.css"
+  },
+  {
+    "revision": "475888dcf99c1ec9fa4a",
+    "url": "/wjs97/wjs97.github.io/blackjack/static/js/2.ff422358.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wjs97/wjs97.github.io/blackjack/static/js/2.ff422358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf8476f0b397ebf56a7",
+    "url": "/wjs97/wjs97.github.io/blackjack/static/js/main.0636ff9a.chunk.js"
+  },
+  {
+    "revision": "7fef7b7bf0a9a2d8456b",
+    "url": "/wjs97/wjs97.github.io/blackjack/static/js/runtime-main.358a787c.js"
+  }
+]);
